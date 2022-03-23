cb33544caea7f28fa1c2bbf4b23cc6ac989cf235
hjy <hongjy1997@naver.com>
2022-03-23 16:24:16 +0900

    수정

--- src/main/java/com/hjy/springwebservice/domain/posts/Posts.java
+++ src/main/java/com/hjy/springwebservice/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.hjy.springwebservice.domain.posts;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,6 +29,7 @@ public class Posts {
 
     //이 Posts클래스는 실제 DB의 테이블과 매칭될 클래스이며 보통 Entity클래스라고도 한다.
     //JPA를 사용하면 DB데이터에 작업할 경우 실제 쿼리를 날리기 보다는, 이 Entity클래스의 수정을 통해 작업한다.
+    @Builder
     public Posts(String title, String content, String author){
         this.title = title;
         this.content = content;
--- src/main/java/com/hjy/springwebservice/domain/posts/PostsRepository.java
+++ src/main/java/com/hjy/springwebservice/domain/posts/PostsRepository.java
@@ -1,4 +1,10 @@
 package com.hjy.springwebservice.domain.posts;
 
-public interface PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//ibatis/Mybatis 등에서 Dao라고 불리느 DB Layer접근자
+//JPA에선 Repository라고 부르며 인터페이스로 생성
+//단순히 인터페이스를 생성후 JpaRepository<Entity클래스, PK타입>를 상속하면 기본적인 CRUD메소드가 자동으로 생성됨
+//특별히 @Repository를 추가할필요도 없음
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
